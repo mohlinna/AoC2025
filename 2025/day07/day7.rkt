87a47d9fd8163c694fef73381cae2b35e8703e31
mohlinna <nathan.mohling@gmail.com>
2026-01-19 17:07:57 -0600

    Day 7 Part 2

--- 2025/day07/day7.rkt
+++ 2025/day07/day7.rkt
@@ -30,10 +30,31 @@
     0))
 
 ;; --- Part 2 ---
+(define (find-quantum-tachyon-splits-loop input current-beams next-beams)
+  (if (empty? input)
+    (sum (map (lambda (x) (cdr x)) current-beams))
+    (if (empty? current-beams)
+      (find-quantum-tachyon-splits-loop (cddr input) (hash->list next-beams) (hash))
+      (let ([current (caar current-beams)]
+            [current-quantity (cdar current-beams)])
+        (if (equal? #\^ (vector-ref (car input) current))
+          (let* ([one-above (+ current 1)]
+                 [one-above-quantity (hash-ref next-beams one-above 0)]
+                 [new-one-above-quantity (+ current-quantity one-above-quantity)]
+                 [one-below (- current 1)]
+                 [one-below-quantity (hash-ref next-beams one-below 0)]
+                 [new-one-below-quantity (+ current-quantity one-below-quantity)])
+            (find-quantum-tachyon-splits-loop input (cdr current-beams) (hash-set (hash-set next-beams one-above new-one-above-quantity) one-below new-one-below-quantity)))
+          (let ([new-current-quantity (+ current-quantity (hash-ref next-beams current 0))])
+            (find-quantum-tachyon-splits-loop input (cdr current-beams) (hash-set next-beams current new-current-quantity))))))))
+
 (define (solve-part2 input)
   ;; Implement logic for Part 2
   (displayln "Solving Part 2...")
-  2)
+  (find-quantum-tachyon-splits-loop
+    (map list->vector (map string->list (cddr input)))
+    (list (cons (string-find (car input) "S") 1))
+    (hash)))
 
 ;; --- Main Execution ---
 (define input-filename "input")
